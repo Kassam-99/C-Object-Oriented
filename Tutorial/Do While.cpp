2bbd770458fd00dfdde7d17f05454e8f7d94a87e
Kassam Dakhlalah <96330526+Kassam-99@users.noreply.github.com>
2022-07-11 13:01:46 +0000

    Create Do While.cpp

---
+++ Tutorial/Do While.cpp
@@ -0,0 +1,11 @@
+/*
+The do/while loop is a variant of the while loop. 
+Before determining whether the condition is true, this loop will run the code block once. If the condition is true, it will then repeat the loop.
+*/
+int main(){
+  do {
+  // code block to be executed
+  }
+  while (condition);
+  return 0;
+}
