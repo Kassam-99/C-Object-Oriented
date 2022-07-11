33f2a0d65919855c8e1ff79ec19ac7de15423f8b
Kassam Dakhlalah <96330526+Kassam-99@users.noreply.github.com>
2022-07-11 10:51:13 +0000

    Update switch statement example.cpp

--- Tutorial/switch statement example.cpp
+++ Tutorial/switch statement example.cpp
@@ -5,15 +5,18 @@ using namespace std;
 
 // Use the switch statement to select one of many code blocks to be executed.
 /*
-This is how it works:
-    The switch expression is evaluated once.
-    The value of the expression is compared with the values of each case.
-    If there is a match, the associated block of code is executed.
-    The break and default keywords are optional.
-    
-    
-When C++ reaches a break keyword, it breaks out of the switch block.
-The example below:
+The process is as follows:
+
+One evaluation of the switch expression is done.
+
+The expression's value and the values in each scenario are contrasted.
+
+The corresponding piece of code is run if there is a match.
+
+The default and break keywords are not required.
+
+C++ exits the switch block when it encounters the break keyword. 
+
 */
 
 
