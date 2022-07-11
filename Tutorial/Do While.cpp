330e7ff3bd4bb86d8e3161793ae025ebeba55d79
Kassam Dakhlalah <96330526+Kassam-99@users.noreply.github.com>
2022-07-11 13:09:32 +0000

    Update Do While.cpp

--- Tutorial/Do While.cpp
+++ Tutorial/Do While.cpp
@@ -1,7 +1,7 @@
 /*
 The do/while loop is a variant of the while loop. 
 Before determining whether the condition is true, this loop will run the code block once. If the condition is true, it will then repeat the loop.
-*/
+
 int main(){
   do {
   // code block to be executed
@@ -9,3 +9,17 @@ int main(){
   while (condition);
   return 0;
 }
+
+
+*/
+//----------------Example No.1----------------:
+int main(){
+  int i = 0;
+  do {
+    cout << i << "\n";
+    i++;
+  }
+  while (i < 5);
+  return 0:
+}
+//Do not forget to increase the variable used in the condition, otherwise the loop will never end.
