de4e281a399e5e155c3646372d2cbd62488821e4
Kassam Dakhlalah <96330526+Kassam-99@users.noreply.github.com>
2022-07-11 11:56:34 +0000

    Create while loop example.cpp

---
+++ Tutorial/while loop example.cpp
@@ -0,0 +1,22 @@
+/*
+The while loop loops through a block of code as long as a specified condition is true:
+
+
+int main()
+{
+    while (condition )
+    {
+        code
+        change
+    }
+
+}
+
+
+When a certain condition is met, a block of code can be executed by a loop.
+
+
+Loops are useful because they speed up programming, lower mistake rates, and improve readability. 
+
+  
+*/
